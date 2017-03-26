93e5604223dab9b318b9b703134137fd453944bb
chosenone75 <1527017530@qq.com>
2017-03-26 11:12:35 +0800

    memory error!!!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Some Notes About Neural Network
 
-This project is some implement of Neural Networks such as vanilla NN,vanilla RNN,CNN\LSTM in tensorflow and so on.
+This project is some implement of Neural Networks such as vanilla NN,vanilla RNN,CNN\LSTM in tensorflow and so on../
---
+++ word2vec/word2vec_demo_with_pre_trained_model.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import word2vec
+
+
+#==============================================================================
+# # word segmented? I guess.
+# word2vec.word2phrase("./text8","./text8-phrases",verbose=True)
+# 
+# # word-phrases to vector
+# word2vec.word2vec("./text8-phrases","./text8.bin",size=100,verbose=True)
+# 
+#==============================================================================
+
+model = word2vec.load("/home/chosenone/download/GoogleNews-vectors-negative300.bin")
+print(model.vocab.shape)
+print(model.vectors.shape)
+
+
